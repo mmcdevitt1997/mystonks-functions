8fd7f7593d940301e877f542e940c527efe56229
Matthew McDevitt <mmcdevitt1997@gmail.com>
2020-12-20 10:36:24 -0600

    sign route is complete

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,24 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
+const app = require('express')()
 
 admin.initializeApp()
 
-const express = require('express');
-const { request, response } = require('express');
-const app = express()
+const firebaseConfig = {
+    apiKey: "AIzaSyCK2Y6kA08nT2yX0V1YaH_Gm1Ls_XlHnRc",
+    authDomain: "my-stonks.firebaseapp.com",
+    databaseURL: "https://my-stonks.firebaseio.com",
+    projectId: "my-stonks",
+    storageBucket: "my-stonks.appspot.com",
+    messagingSenderId: "108739936062",
+    appId: "1:108739936062:web:362b865d41155c0b9df66f",
+    measurementId: "G-EW5N2JNJXG"
+  }
 
+
+const firebase = require('firebase');
+const { request, response } = require('express');
+firebase.initializeApp(firebaseConfig)
 
 app.get('/posts', (request, response) => {
     admin.firestore().collection('posts').get()
@@ -39,6 +51,27 @@ app.post('/post', (request, response) => {
         })
          
 }) 
+
+// Signup route 
+app.post('/signup', (request, response) => {
+    const newUser = {
+        email: request.body.email,
+        password: request.body.password,
+        confirmPassword: request.body.confirmPassword,
+        userName: request.body.userName
+    }
+    // TODO: Validate the data 
+
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then(data => {
+           return response.status(201).json({ message: `user ${data.user.uid} signed up successfully`})
+            
+        })
+        .catch(err => {
+            console.error(err)
+            return response.status(500).json({error: err.code})
+        })
+})
 
 
 exports.api = functions.https.onRequest(app)
