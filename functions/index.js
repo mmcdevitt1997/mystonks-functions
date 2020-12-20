8d8ab278548dcb0203ed835d367663d36adec102
Matthew McDevitt <mmcdevitt1997@gmail.com>
2020-12-19 21:25:42 -0600

    first commit

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+
+admin.initializeApp()
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello world");
+});
+
+exports.getPosts = functions.https.onRequest((request, response) => {
+    admin.firestore().collection('posts').get()
+        .then(data => {
+            let posts = []
+            data.forEach(doc => {
+                posts.push(doc.data())
+            })
+            return response.json(posts)
+        })
+        .catch((err) => console.error(err))
+}) 
