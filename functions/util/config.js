669b2bcb813e8009f5f3db03bbb8d42c01a2c3da
Matthew McDevitt <mmcdevitt1997@gmail.com>
2020-12-21 08:07:55 -0600

    work

--- functions/util/config.js
+++ functions/util/config.js
@@ -3,8 +3,8 @@ module.exports = {
     authDomain: "my-stonks.firebaseapp.com",
     databaseURL: "https://my-stonks.firebaseio.com",
     projectId: "my-stonks",
-    storageBucket: "my-stonks.appspot.cm",
+    storageBucket: "my-stonks.appspot.com",
     messagingSenderId: "108739936062",
     appId: "1:108739936062:web:362b865d41155c0b9df66f",
-    measurementId: "G-EW5N2JNJXG" 
+    measurementId: "G-EW5N2JNJXG"
   }
